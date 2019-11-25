0e54410aa66ec31a8b0ddbeaeeda7120ebc6fd6c
Kevin Brockhoff <kbrockhoff@codekaizen.org>
2019-11-25 17:18:13 -0600

    initial dev of Throwable to ErrorData translator

---
+++ src/main/java/io/opentelemetry/futureapi/events/ThrowableTranslator.java
@@ -0,0 +1,251 @@
+/*
+ * Copyright 2019, OpenTelemetry Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.opentelemetry.futureapi.events;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.common.base.Strings.isNullOrEmpty;
+import static com.google.common.base.Strings.padStart;
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+import com.google.protobuf.Any;
+import com.google.protobuf.GeneratedMessageV3;
+import io.opentelemetry.proto.common.v1.AttributeKeyValue;
+import io.opentelemetry.proto.events.v1.ErrorData;
+import io.opentelemetry.proto.events.v1.Exception;
+import io.opentelemetry.proto.events.v1.StackTrace;
+import java.lang.reflect.InvocationTargetException;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.nio.ByteBuffer;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.time.Instant;
+import java.util.Map;
+import io.opentelemetry.proto.events.v1.Event;
+import java.util.UUID;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Converts a {@link Throwable} with potentially nested throwables into an OpenTelementry {@code
+ * error} event.
+ */
+public class ThrowableTranslator {
+
+  public static final String EVENT_ERROR = "error";
+  public static final String ATTR_ERROR_KIND = "error.kind";
+  public static final String ATTR_ERROR_OBJECT = "error.object";
+  public static final String ATTR_ERROR_MESSAGE = "message";
+  public static final String ATTR_ERROR_STACK = "stack";
+  private static final int DEFAULT_MAX_STACK_TRACE_LENGTH = 50;
+  private static final Logger LOGGER = Logger.getLogger(ThrowableTranslator.class.getName());
+
+  private int maxStackTraceLength;
+
+  public ThrowableTranslator() {
+    this(DEFAULT_MAX_STACK_TRACE_LENGTH);
+  }
+
+  public ThrowableTranslator(int maxStackTraceLength) {
+    this.maxStackTraceLength = maxStackTraceLength;
+  }
+
+  public Event translateThrowable(Throwable source, Map<String, Object> parameters) {
+    checkNotNull(source, "source is required");
+    Event.Builder builder = Event.newBuilder();
+    Instant ts = Instant.now();
+    builder
+        .setTimeUnixnano(ts.getEpochSecond() * 1000000000L + ts.getNano())
+        .setDescription(EVENT_ERROR)
+        .addAttributes(buildStringAttribute(
+            ATTR_ERROR_MESSAGE, constructMessage(source)))
+        .addAttributes(buildAnyAttribute(
+            ATTR_ERROR_OBJECT, constructErrorData(source, parameters)));
+    return builder.build();
+  }
+
+  private String constructMessage(Throwable throwable) {
+    String message = throwable.getLocalizedMessage();
+    if (isNullOrEmpty(message)) {
+      message = throwable.getClass().getName();
+    }
+    return message;
+  }
+
+  private ErrorData constructErrorData(Throwable throwable, Map<String, Object> arguments) {
+    ErrorData.Builder builder = ErrorData.newBuilder();
+    MessageDigest hash = newMessageDigest();
+    MessageDigest issue = newMessageDigest();
+    String id = generateId();
+    Exception.Builder exception = constructException(throwable, id, hash, issue);
+    Throwable nextNode = extractCause(throwable);
+    while (null != nextNode) {
+      final Throwable currentNode = nextNode;
+      id = generateId();
+      exception.setCause(id);
+      builder.addExceptions(exception.build());
+      exception = constructException(currentNode, id, hash, issue);
+      nextNode = extractCause(currentNode);
+      if (currentNode.equals(nextNode)) {
+        nextNode = null;
+      }
+    }
+    builder.addExceptions(exception.build());
+    if (arguments != null) {
+      addArguments(builder, hash, arguments);
+    }
+    builder.setHashId(hashToString(hash));
+    builder.setIssueHashId(hashToString(issue));
+    return builder.build();
+  }
+
+  private Exception.Builder constructException(
+      Throwable throwable, String id, MessageDigest hash, MessageDigest issue) {
+    calculateInstanceHash(throwable, hash);
+    calculateIssueHash(throwable, issue);
+    Exception.Builder builder = Exception.newBuilder();
+    builder.setId(id);
+    if (!isNullOrEmpty(throwable.getMessage())) {
+      builder.setMesssage(throwable.getMessage());
+    }
+    builder.setType(throwable.getClass().getName());
+    builder.setStack(constructStackTrace(throwable.getStackTrace()));
+    return builder;
+  }
+
+  private StackTrace constructStackTrace(StackTraceElement[] elements) {
+    StackTrace.Builder builder = StackTrace.newBuilder();
+    MessageDigest hash = newMessageDigest();
+    if (elements.length > maxStackTraceLength) {
+      for (int i = 0; i < maxStackTraceLength; i++) {
+        builder.addFrames(constructStackFrame(elements[i]));
+        hash.update(elements[i].toString().getBytes(UTF_8));
+      }
+      builder.setDroppedFramesCount(elements.length - maxStackTraceLength);
+    } else {
+      for (int i = 0; i < elements.length; i++) {
+        builder.addFrames(constructStackFrame(elements[i]));
+        hash.update(elements[i].toString().getBytes(UTF_8));
+      }
+    }
+    builder.setStackTraceHashId(hashToString(hash));
+    return builder.build();
+  }
+
+  private StackTrace.StackFrame constructStackFrame(StackTraceElement element) {
+    return StackTrace.StackFrame.newBuilder()
+        .setFunctionName(element.getClassName() + "." + element.getMethodName())
+        .setFileName(isNullOrEmpty(element.getFileName()) ? "" : element.getFileName())
+        .setLineNumber(element.getLineNumber())
+        .build();
+  }
+
+  private static String generateId() {
+    return UUID.randomUUID().toString();
+  }
+
+  private static void addArguments(
+      ErrorData.Builder builder, MessageDigest hash, Map<String, Object> arguments) {
+    for (Map.Entry<String, Object> entry : arguments.entrySet()) {
+      if (entry.getValue() == null) {
+        continue;
+      }
+      hash.update(entry.getKey().getBytes(UTF_8));
+      if (entry.getValue() instanceof Number) {
+        Number number = (Number) entry.getValue();
+        if (number instanceof Double || number instanceof Float || number instanceof BigDecimal) {
+          builder.addArguments(buildDoubleAttribute(entry.getKey(), number.doubleValue()));
+          ByteBuffer buffer = ByteBuffer.allocate(Double.BYTES);
+          buffer.putDouble(number.doubleValue());
+          hash.update(buffer);
+        } else {
+          builder.addArguments(buildIntAttribute(entry.getKey(), number.longValue()));
+          ByteBuffer buffer = ByteBuffer.allocate(Long.BYTES);
+          buffer.putLong(number.longValue());
+          hash.update(buffer);
+        }
+      } else if (entry.getValue() instanceof Boolean) {
+        Boolean value = (Boolean) entry.getValue();
+        builder.addArguments(buildBoolAttribute(entry.getKey(), value.booleanValue()));
+        hash.update((byte) (value.booleanValue() ? 1 : 0));
+      } else {
+        String value = entry.getValue().toString();
+        builder.addArguments(buildStringAttribute(entry.getKey(), value));
+        hash.update(value.getBytes(UTF_8));
+      }
+    }
+  }
+
+  private static AttributeKeyValue buildStringAttribute(String key, String value) {
+    return AttributeKeyValue.newBuilder().setKey(key).setStringValue(value).build();
+  }
+
+  private static AttributeKeyValue buildIntAttribute(String key, long value) {
+    return AttributeKeyValue.newBuilder().setKey(key).setIntValue(value).build();
+  }
+
+  private static AttributeKeyValue buildDoubleAttribute(String key, double value) {
+    return AttributeKeyValue.newBuilder().setKey(key).setDoubleValue(value).build();
+  }
+
+  private static AttributeKeyValue buildBoolAttribute(String key, boolean value) {
+    return AttributeKeyValue.newBuilder().setKey(key).setBoolValue(value).build();
+  }
+
+  private static AttributeKeyValue buildAnyAttribute(String key, GeneratedMessageV3 value) {
+    String typeUrl = "type.googleapis.com/" + value.getDescriptorForType().getFullName();
+    Any any = Any.newBuilder().setTypeUrl(typeUrl).setValue(value.toByteString()).build();
+    return AttributeKeyValue.newBuilder().setKey(key).setAnyValue(any).build();
+  }
+
+  private static Throwable extractCause(Throwable throwable) {
+    if (throwable instanceof InvocationTargetException) {
+      return ((InvocationTargetException) throwable).getTargetException();
+    } else {
+      return throwable.getCause();
+    }
+  }
+
+  private static void calculateInstanceHash(Throwable throwable, MessageDigest hash) {
+    hash.update(throwable.getClass().getName().getBytes(UTF_8));
+    hash.update(throwable.getStackTrace()[0].toString().getBytes(UTF_8));
+    if (!isNullOrEmpty(throwable.getMessage())) {
+      hash.update(throwable.getMessage().getBytes(UTF_8));
+    }
+  }
+
+  private static void calculateIssueHash(Throwable throwable, MessageDigest hash) {
+    hash.update(throwable.getClass().getName().getBytes(UTF_8));
+    hash.update(throwable.getStackTrace()[0].toString().getBytes(UTF_8));
+  }
+
+  private static MessageDigest newMessageDigest() {
+    try {
+      return MessageDigest.getInstance("MD5");
+    } catch (NoSuchAlgorithmException willNeverBeThrown) {
+      LOGGER.log(Level.INFO, "unexpected exception", willNeverBeThrown);
+      throw new IllegalStateException(willNeverBeThrown);
+    }
+  }
+
+  private static String hashToString(MessageDigest hash) {
+    byte[] messageDigest = hash.digest();
+    BigInteger no = new BigInteger(1, messageDigest);
+    return padStart(no.toString(16), 32, '0');
+  }
+
+}
---
+++ src/test/java/io/opentelemetry/futureapi/events/ThrowableTranslatorTest.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright 2019, OpenTelemetry Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.opentelemetry.futureapi.events;
+
+import static io.opentelemetry.futureapi.events.ThrowableTranslator.EVENT_ERROR;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import com.google.protobuf.Any;
+import com.google.protobuf.InvalidProtocolBufferException;
+import io.opentelemetry.proto.events.v1.ErrorData;
+import io.opentelemetry.proto.events.v1.Event;
+import java.sql.SQLException;
+import java.sql.SQLIntegrityConstraintViolationException;
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Logger;
+import org.junit.Test;
+
+/** Unit tests for {@link ThrowableTranslator}. */
+public class ThrowableTranslatorTest {
+
+  private static final Logger LOGGER = Logger.getLogger(ThrowableTranslatorTest.class.getName());
+
+  @Test
+  public void shouldTranslateExceptionWithNoMessageNoNestedCausesAndNullParameters() {
+    ThrowableTranslator translator = new ThrowableTranslator();
+    Exception throwable = new NullPointerException();
+    Event event = translator.translateThrowable(throwable, null);
+    LOGGER.info(event.toByteString().toStringUtf8());
+    validateEventTime(event);
+    assertEquals(EVENT_ERROR, event.getDescription());
+    assertEquals(2, event.getAttributesCount());
+  }
+
+  @Test
+  public void shouldTranslateExceptionWithMessageNestedCauseAndParameters()
+      throws InvalidProtocolBufferException {
+    ThrowableTranslator translator = new ThrowableTranslator();
+    Exception throwable = generateMultiCauseException();
+    Map<String, Object> parameters = new HashMap<>();
+    parameters.put("personId", 58763);
+    parameters.put("firstName", "Kent");
+    parameters.put("lastName", "Beck");
+    parameters.put("averageRating", 4.87);
+    parameters.put("participant", true);
+    Event event = translator.translateThrowable(throwable, parameters);
+    LOGGER.info(event.toByteString().toStringUtf8());
+    validateEventTime(event);
+    assertEquals(EVENT_ERROR, event.getDescription());
+    assertEquals(2, event.getAttributesCount());
+    Any any = event.getAttributesList().get(1).getAnyValue();
+    ErrorData errorData = ErrorData.parseFrom(any.getValue());
+    assertEquals(2, errorData.getExceptionsCount());
+    assertEquals(5, errorData.getArgumentsCount());
+  }
+
+  private void validateEventTime(Event event) {
+    Instant timestamp = Instant.ofEpochSecond(event.getTimeUnixnano() / 1000000000L,
+        event.getTimeUnixnano() % 1000000000L);
+    Instant current = Instant.now();
+    assertTrue(timestamp.getEpochSecond() <= current.getEpochSecond() &&
+        timestamp.getEpochSecond() > current.getEpochSecond() - 60);
+  }
+
+  private Exception generateMultiCauseException() {
+    try {
+      callThatThrowsNestedIllegalArgumentException();
+    } catch (Exception exception) {
+      return exception;
+    }
+    return null;
+  }
+
+  private void callThatThrowsNestedIllegalArgumentException() {
+    try {
+      callThatThrowsSQLException();
+    } catch (SQLException cause) {
+      throw new IllegalArgumentException("invalid input data", cause);
+    }
+  }
+
+  private void callThatThrowsSQLException() throws SQLException {
+    throw new SQLIntegrityConstraintViolationException(
+        "Column widget_id cannot be null", "23000", 1048);
+  }
+}
